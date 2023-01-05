add4f5f654088260051464f8eb234e46c6210cb2
John Lopez <Lopez.thinktank@gmail.com>
2023-01-05 13:03:52 -0800

    completed the DB files, schema and seeds. Started my work on the server.js file.

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
-const db = require('./db/connection');
+// Including app dependencies
+// const mysql = require('mysql');
+const db = require('./db/schema.sql')
 const inquirer = require('inquirer');
-require("console.table");
+const cTable = require("console.table");
+
+const connection = mysql.createConnection({
+    host:'localhost',
+    port: 3306,
+    database: 'employeeDb'
+});
 
 
