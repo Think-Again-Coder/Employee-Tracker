961c379259614b8345801dc238f26ceee7cbb82c
unknown <lopez.thinktank@gmail.com>
2023-01-07 16:29:31 -0800

    updated the server.js file to include prompts for app and installed the missing modules which included the package.json file

--- server.js
+++ server.js
@@ -1,13 +1,41 @@
 // Including app dependencies
-// const mysql = require('mysql');
-const db = require('./db/schema.sql')
+const mysql2 = require('mysql2');
+const db = require('./db');
 const inquirer = require('inquirer');
 const cTable = require("console.table");
 
-const connection = mysql.createConnection({
-    host:'localhost',
-    port: 3306,
-    database: 'employeeDb'
+// const connection = mysql2.createConnection({
+//     host:'localhost',
+//     port: 3306,
+//     password: '',
+//     database: 'employeedb'
+// });
+
+function startPrompts() {
+    inquirer.prompt([
+        {
+        type: 'list',
+        name: 'start',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Employees',
+            'View All Roles',
+            'View All Departments',
+            'Add an Employee',
+            'Add a Role',
+            'Add a Department'
+        ]
+    },
+        
+]).then((choices) => {
+    const answers = choices.start;
+    //if statements start here
+    if (answers === 'View All Employees') {
+        viewDepartments();
+    } else if (answers === 'View All Roles') {
+        viewRoles();
+    }
 });
 
-
+}
+startPrompts();
